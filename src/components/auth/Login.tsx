9d3ccc1f6172e07f254f74aa37ff4b69dbc91691
5iveemmy <ainemmanuel87@gmail.com>
2022-10-08 14:30:48 +0100

    chore: add Avenir font

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,12 @@
+import styles from "./Login.module.scss";
+
+const Login = () => {
+  return (
+    <div className={styles.layout}>
+      <h2>Auth</h2>
+      <p>What the fuck</p>
+    </div>
+  );
+};
+
+export default Login;
